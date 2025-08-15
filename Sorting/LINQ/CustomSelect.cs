2b62b680f6dfd67f2a7062d67e9f8ed082dc4127
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-08-16 03:07:47 +0500

    Where and Select Custom

---
+++ Sorting/LINQ/CustomSelect.cs
@@ -0,0 +1,11 @@
+﻿namespace Sorting.LINQ;
+
+public static class CustomSelect
+{
+    public static IEnumerable<TResult> MySelect<T, TResult>(this IEnumerable<T> source,
+        Func<T, TResult> selector)
+    {
+        foreach(var item in source)
+            yield return selector(item);
+    }
+}
---
+++ Sorting/LINQ/CustomWhere.cs
@@ -0,0 +1,12 @@
+﻿namespace Sorting.LINQ;
+
+public static class CustomWhere
+{
+    public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source,
+        Func<T, bool> predicate)
+    {
+        foreach(var item in source)
+            if(predicate(item))
+                yield return item;
+    }
+}
